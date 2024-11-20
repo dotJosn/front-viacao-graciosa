e83b25e758b14d39c5ca95120b7b4b021dd4f052
Adrian Netto <114098348+AdrianNetto@users.noreply.github.com>
2024-11-20 14:21:27 -0300

    feat: adding chat and some responsivity (#10)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,20 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { IoMdHome } from "react-icons/io";
 import { FaBusAlt } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     console.log(window.innerWidth);
   }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <header className="bg-graciosa text-white py-6 shadow-lg">
@@ -15,8 +22,17 @@ const Header = () => {
         <Link href="/home" className="text-2xl font-extrabold tracking-wide">
           Viação Graciosa
         </Link>
-        <nav>
-          <ul className="flex space-x-8">
+        <div className="lg:hidden">
+          <button onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <FaTimes className="text-2xl" />
+            ) : (
+              <FaBars className="text-2xl" />
+            )}
+          </button>
+        </div>
+        <nav className={`${isMenuOpen ? "block" : "hidden"} lg:block`}>
+          <ul className="flex space-x-8 lg:flex-row flex-col lg:space-x-8 space-y-4 lg:space-y-0">
             <li className="flex items-center space-x-2 group">
               <IoMdHome className="text-2xl group-hover:text-yellow-300 transition-colors duration-300" />
               <Link href="/home" className="text-lg font-medium hover:text-yellow-300 transition-colors duration-300">
